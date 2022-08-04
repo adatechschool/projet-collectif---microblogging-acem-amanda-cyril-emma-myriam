c1e12c3b3b3fe5b7cb8bc3881ac25012a7fd0b3c
Myriam-Mnnr <98812962+Myriam-Mnnr@users.noreply.github.com>
2022-08-04 15:58:18 +0200

    Fixed Post controller and view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post; 
 
 class PostController extends Controller
 {
-    //
+    public function show()
+    {
+    $post = Post::all(); 
+    //dump($posts); 
+    
+    return view('posts.show', ['post' => $post[0]]);
+    }
 }
