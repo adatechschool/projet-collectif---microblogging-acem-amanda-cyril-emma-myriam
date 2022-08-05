460b338973b15c9efffe095de74e39e9f51d8a8c
Cyril <lgcyriltt@gmail.com>
2022-08-05 14:53:10 +0200

    corrections posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,23 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Post; 
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function show($id)
     {
-    $post = Post::all(); 
-<<<<<<< HEAD
-   // dump($post); 
-    
-    return view('posts.show', ['post' => $post[$id - 1]]);
+        $post = Post::all();
+
+        // dump($post); 
+
+        return view('posts.show', ['post' => $post[$id - 1]]);
     }
 }
-=======
-    //dump($posts); 
-    
-    return view('posts.show', ['post' => $post[$id - 1]]);
-    }
-}
->>>>>>> d7b122b2fe8d025057ae2c3d9b52852985fe8ca5
