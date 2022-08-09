95024bc73f79faa9feb9d60fdf8782b56960dec3
Myriam-Mnnr <98812962+Myriam-Mnnr@users.noreply.github.com>
2022-08-09 15:40:31 +0200

    Page profil Cyril sushis

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,5 +1,13 @@
-<x-guest-layout>
-    <h1>HEY</h1>
-  <h2>{{$user->name}}</h2>
+<x-app-layout>
+<x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Profil') }}
+        </h2>
+    </x-slot>
+
+    
+    <img class="w-1/5 rounded-full" src="https://play-lh.googleusercontent.com/XVHP0sBKrRJYZq_dB1RalwSmx5TcYYRRfYMFO18jgNAnxHAIA1osxM55XHYTb3LpkV8" />
+  <h2 class="font-bold">{{$user->name}}</h2>
   <p>{{$user->biography}}</p>
-</x-guest-layout>
+  <p>On this site since {{$user->created_at}}</p>
+</x-app-layout>
